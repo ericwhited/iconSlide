71c9425f5032c579a98fbde54dce881e15cccd63
Eric <eejw93@gmail.com>
2019-06-11 11:23:24 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
     $('.navList-item').eq(0).addClass('is-active');
     
-    // $('.navList-item').each(function(element){
-    //     $('.navList-item').removeClass('is-active');
-    // })
-
     $('.navList-item').click(function(){
         $('.navList-item').removeClass('is-active');
         $(this).addClass('is-active');
